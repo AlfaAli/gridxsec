b37e1ed7b5666091823588c01d771895abdc992b
Knut Liseter <knut.lisaeter@nersc.no>
2016-11-18 13:02:13 +0100

    ...

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "abfile",
+    name = "gridxsec",
     version = "0.0.1",
     author = "Knut Lisaeter",
     author_email = "knutal@gmail.com",
